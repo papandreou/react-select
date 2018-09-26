9bb8ed2ba6298b76ff39f4c041a15d2e7d209f85
Andreas Lind <andreas.lind@peakon.com>
2018-09-26 16:33:04 +0200

    Add an 'ignore order' mode where multiple words in the input are allowed to be found in any order

--- packages/react-select/src/filters.js
+++ packages/react-select/src/filters.js
@@ -3,6 +3,7 @@
 type Config = {
   ignoreCase?: boolean,
   ignoreAccents?: boolean,
+  ignoreOrder?: boolean,
   stringify?: Object => string,
   trim?: boolean,
   matchFrom?: 'any' | 'start' | 'start-word',
@@ -13,13 +14,37 @@ import { stripDiacritics } from './diacritics';
 const trimString = str => str.replace(/^\s+|\s+$/g, '');
 const defaultStringify = option => `${option.label} ${option.value}`;
 
+function matchCandidate(input, candidate, matchFrom) {
+  if (matchFrom === 'start') {
+    return candidate.substr(0, input.length) === input;
+  } else if (matchFrom === 'start-word') {
+    const matchIndex = candidate.indexOf(input);
+    // Accept a match at the start of the string or one preceded by space:
+    return (
+      matchIndex === 0 ||
+      (matchIndex > 0 && /\s/.test(candidate.charAt(matchIndex - 1)))
+    );
+  } else {
+    // matchFrom === 'any'
+    return candidate.indexOf(input) > -1;
+  }
+}
+
 export const createFilter = (config: ?Config) => (
   option: { label: string, value: string, data: any },
   rawInput: string
 ): boolean => {
-  const { ignoreCase, ignoreAccents, stringify, trim, matchFrom } = {
+  const {
+    ignoreCase,
+    ignoreAccents,
+    ignoreOrder,
+    stringify,
+    trim,
+    matchFrom,
+  } = {
     ignoreCase: true,
     ignoreAccents: true,
+    ignoreOrder: false,
     stringify: defaultStringify,
     trim: true,
     matchFrom: 'any',
@@ -35,17 +60,18 @@ export const createFilter = (config: ?Config) => (
     input = stripDiacritics(input);
     candidate = stripDiacritics(candidate);
   }
-  if (matchFrom === 'start') {
-    return candidate.substr(0, input.length) === input;
-  } else if (matchFrom === 'start-word') {
-    const matchIndex = candidate.indexOf(input);
-    // Accept a match at the start of the string or one preceded by space:
-    return (
-      matchIndex === 0 ||
-      (matchIndex > 0 && /\s/.test(candidate.charAt(matchIndex - 1)))
-    );
+
+  if (ignoreOrder) {
+    return input
+      .split(/\s+/)
+      .every(inputWord =>
+        candidate
+          .split(/\s+/)
+          .some(candidateWord =>
+            matchCandidate(inputWord, candidateWord, matchFrom)
+          )
+      );
   } else {
-    // matchFrom === 'any'
-    return candidate.indexOf(input) > -1;
+    return matchCandidate(input, candidate, matchFrom);
   }
 };
